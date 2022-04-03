172ac6dc39b4aeec78978f866e7dab2169ac7723
beninsydney <benodev@protonmail.com>
2022-04-03 17:09:21 +1000

    WIP

--- dashboard-server/src/proxy/x-account.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (req, proxyRequestOptions) => {
-  if (req.urlPath === '/' ||
-      req.urlPath === '/favicon.ico' ||
-      req.urlPath === '/robots.txt' ||
-      req.urlPath.startsWith('/public/') || !req.account) {
-    return
-  }
-  proxyRequestOptions.headers['x-account'] = JSON.stringify(req.account)
-}
--- dashboard-server/src/server/allow-api-request.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  before: async (req) => {
-    if (!req.urlPath.startsWith('/api/')) {
-      return
-    }
-    req.allowAPIRequest = global.sitemap[req.urlPath] === undefined
-  }
-}
