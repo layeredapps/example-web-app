98f2c29ada7a7debc15ff08e2e295cb49c9dad00
beninsydney <benodev@protonmail.com>
2022-04-30 11:00:10 +1000

    Don't save screenshots or API responses

--- dashboard-server/tests.sh
+++ dashboard-server/tests.sh
@@ -5,4 +5,8 @@ APPLICATION_SERVER_TOKEN=token \
 DASHBOARD_SERVER=http://localhost:9200 \
 START_APPLICATION_SERVER=false \
 APPLICATION_SERVER_PORT=3000 \
+GENERATE_SCREENSHOTS=false \
+SCREENSHOT_PATH=/home/appstore-project/workspace/dashboard/layeredapps.github.io/screenshots/example-web-app \
+GENERATE_RESPONSE=false \
+RESPONSE_PATH=/tmp \
 npm run deploy-check
